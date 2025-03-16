a28796c408c21b5c718149877158bb0ad1c2a874
SimonJayed <simonjay.lugatiman@cit.edu>
2025-03-16 16:25:59 +0800

    HollowBOUND HUZZAAA

---
+++ src/main/Sound.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+public class Sound {
+
+    private Clip clip;
+    private URL soundURL[] = new URL[30];
+    private FloatControl volumeControl;
+
+
+    public Sound(){
+        try {
+            soundURL[0] = getClass().getResource("/sound/LetterBee.wav");
+            soundURL[1] = getClass().getResource("/sound/AnimalCrossingTalkingSound.wav");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setFile(int i){
+
+        try {
+            if (soundURL[i] == null) {
+                System.out.println("Invalid sound index: " + i);
+                return;
+            }
+
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+
+            volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error to debug issues
+        }
+    }
+
+    public void play(){
+        clip.start();
+    }
+
+    public void loop(int i){
+        clip.loop(i);
+    }
+
+    public void stop(){
+        clip.stop();
+    }
+
+    public void setVolume(float volume){
+        if (volumeControl != null){
+            float min = volumeControl.getMinimum();
+            float max = volumeControl.getMaximum();
+            float newVolume = Math.min(Math.max(volume, min), max);
+            volumeControl.setValue(newVolume);
+        }
+    }
+
+}
